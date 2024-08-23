8e2e20f619400893a9738f50e43bdf5c46c8777d
mo3tasemjarar3a <s12112082@stu.najah.edu>
2024-08-23 11:09:17 +0300

    Added new features and some edits

---
+++ src/main/java/MySystem/Order.java
@@ -0,0 +1,68 @@
+package MySystem;
+
+public class Order {
+    private String id;
+    private String recipient;
+    private String phone;
+    private String address;
+    private String status;
+    private String supervisor;
+
+    public Order(String id, String recipient, String phone, String address, String status, String supervisor) {
+        this.id = id;
+        this.recipient = recipient;
+        this.phone = phone;
+        this.address = address;
+        this.status = status;
+        this.supervisor = supervisor;
+
+    }
+
+    public String getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(String supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        status = status;
+    }
+}
---
+++ src/test/java/najah/edu/acceptance_test/MessagingSys.java
@@ -0,0 +1,41 @@
+package najah.edu.acceptance_test;
+
+import MySystem.User;
+import MySystem.sweetSys;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MessagingSys {
+    private final MySystem.sweetSys sweetSys;
+
+
+
+    public MessagingSys() {
+        this.sweetSys = new sweetSys();
+    }
+
+
+    @Given("I am logged in as a {string}")
+    public void i_am_logged_in_as_a(String role) {
+        assertEquals("User is an owner", role,"owner");
+
+    }
+
+    @When("I use the messaging system to communicate with {string}")
+    public void i_use_the_messaging_system_to_communicate_with(String recipient) {
+        assertEquals("Recipient is a supplier", recipient, sweetSys.checkRecipient(recipient));
+
+    }
+
+    @Then("I should see the message sent to {string}")
+    public void i_should_see_the_message_sent_to(String recipient) {
+        User u1= new User();
+        u1.setRole(recipient);
+        assertTrue("The message was sent to the recipient",sweetSys.receivedCheck(u1));
+    }
+
+}
